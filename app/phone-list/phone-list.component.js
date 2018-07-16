938ebe306b38f232df6536ea4b2919e3a2b65a80
Orifjon Narkulov <orifjon9@gmail.com>
2018-07-16 10:13:49 -0600

    Two-way Data Binding and Unit testing

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,13 +5,18 @@ angular
         controller: function PhoneListController() {
             this.phones = [{
                 name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.'
+                snippet: 'Fast just got faster with Nexus S.',
+                age: 2015
             }, {
                 name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.'
+                snippet: 'The Next, Next Generation tablet.',
+                age: 2016
             }, {
                 name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.'
+                snippet: 'The Next, Next Generation tablet.',
+                age: 2013
             }];
+
+            this.orderProp = 'name';
         }
     });
